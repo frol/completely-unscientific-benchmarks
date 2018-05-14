3fa0c20097a96f1ed5f2cba8bbd2c7b03b68e0a0
Alexei Matveev <alexei.matveev@gmail.com>
2018-05-14 21:25:12 +0200

    AM: try AOT with Graal

--- clojure/project.clj
+++ clojure/project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main bench.core
+  :profiles {:uberjar {:aot :all}}
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"])
