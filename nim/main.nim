1c246ff2842b95abd4ff94f1539d7d5dc4c26e97
hlaaf <hlaaftana@users.noreply.github.com>
2018-05-14 21:31:07 +0300

    rebase with master

--- nim/main.nim
+++ nim/main.nim
@@ -1,15 +1,13 @@
 import random
 
 type Node = ref object
-  x: int
-  y: int
-  left: Node
-  right: Node
+  x, y: int32
+  left, right: Node
 
-proc new_node(x: int): Node {.noinit.}=
+proc new_node(x: int32): Node {.noinit.}=
   new result
   result.x = x
-  result.y = rand(2147483647)
+  result.y = rand(high(int32).int).int32
   result.left = nil
   result.right = nil
 
@@ -25,7 +23,7 @@ proc merge(lower: Node, greater: Node): Node {.noinit.}=
     greater.left = merge(lower, greater.left)
     return greater
 
-proc splitBinary(orig: Node, value: int): (Node, Node) {.noinit.}=
+proc splitBinary(orig: Node, value: int32): (Node, Node) {.noinit.}=
   if orig.isNil:
     return (nil, nil)
   elif orig.x < value:
@@ -40,7 +38,7 @@ proc splitBinary(orig: Node, value: int): (Node, Node) {.noinit.}=
 proc merge3(lower: Node, equal: Node, greater: Node): Node {.noInit.}=
   result = merge(merge(lower, equal), greater)
 
-proc split(orig: Node, value: int): tuple[lower, equal, greater: Node] =
+proc split(orig: Node, value: int32): tuple[lower, equal, greater: Node] =
   let (lower, equalGreater) = splitBinary(orig, value)
   result.lower = lower
   (result.equal, result.greater) = splitBinary(equalGreater, value + 1)
@@ -48,24 +46,24 @@ proc split(orig: Node, value: int): tuple[lower, equal, greater: Node] =
 type Tree = ref object
   root: Node
 
-proc has_value(self: Tree, x: int): bool =
+proc hasValue(self: Tree, x: int32): bool =
   let splited = split(self.root, x)
   result = not splited.equal.isNil
   self.root = merge3(splited.lower, splited.equal, splited.greater)
 
-proc insert(self: Tree, x: int) =
+proc insert(self: Tree, x: int32) =
   var splited = split(self.root, x)
   if splited.equal.isNil:
     splited.equal = new_node(x)
   self.root = merge3(splited.lower, splited.equal, splited.greater)
 
-proc erase(self: Tree, x: int) =
+proc erase(self: Tree, x: int32) =
   let splited = split(self.root, x)
   self.root = merge(splited.lower, splited.greater)
 
 proc main() =
   let tree = Tree()
-  var cur = 5
+  var cur = 5'i32
   var res = 0
 
   for i in 1 ..< 1000000:
@@ -77,7 +75,7 @@ proc main() =
     of 1:
       tree.erase(cur)
     of 2:
-      if tree.has_value(cur):
+      if tree.hasValue(cur):
         res += 1
     else:
       continue
