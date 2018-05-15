cc41750ae39bcae7bb9cf303704b199a275d687d
Alexei Matveev <alexei.matveev@gmail.com>
2018-05-15 21:12:49 +0200

    AM: omit parens on destructuring

--- fsharp/main.fsx
+++ fsharp/main.fsx
@@ -12,10 +12,10 @@ let make_node (x: int) =
   Some({x = x; y = rand_int (); left = None; right = None})
 
 let rec merge2 lower greater =
-  match (lower, greater) with
-  | (None, greater) -> greater
-  | (lower, None) -> lower
-  | (Some(lo), Some(gt)) ->
+  match lower, greater with
+  | None, greater -> greater
+  | lower, None -> lower
+  | Some(lo), Some(gt) ->
       if lo.y < gt.y
       then Some({lo with right = merge2 lo.right greater})
       else Some({gt with left = merge2 lower gt.left})
@@ -25,32 +25,32 @@ let rec splitBinary orig value =
   | None -> (None, None)
   | Some(orig) ->
       if orig.x < value then
-        let (fst, snd) = splitBinary orig.right value
+        let fst, snd = splitBinary orig.right value
         in (Some({orig with right = fst}), snd)
       else
-        let (fst, snd) = splitBinary orig.left value
+        let fst, snd = splitBinary orig.left value
         in (fst, Some({orig with left = snd}))
 
 let merge3 lower equal greater = merge2 (merge2 lower equal) greater
 
 let split orig value =
-  let (lower, equal_greater) = splitBinary orig value in
-  let (equal, greater) = splitBinary equal_greater (value + 1) in
+  let lower, equal_greater = splitBinary orig value in
+  let equal, greater = splitBinary equal_greater (value + 1) in
   (lower, equal, greater)
 
 // printfn "%A" (split (merge2 (make_node 42) (make_node 43)) 43)
 
 let has_value root x =
-  let (lower, equal, greater) = split root x in
+  let lower, equal, greater = split root x in
   // FIXME: splitting just to merge it again:
   ((merge3 lower equal greater), equal <> None)
 
 let insert root x =
-  let (lower, equal, greater) = split root x in
+  let lower, equal, greater = split root x in
   (merge3 lower (if equal <> None then equal else make_node x) greater)
 
 let erase root x =
-  let (lower, _, greater) = split root x in
+  let lower, _, greater = split root x in
   merge2 lower greater
 
 let main n =
@@ -62,7 +62,7 @@ let main n =
         match i % 3 with
         | 0 -> loop (insert root cur) (i + 1) cur res
         | 1 -> loop (erase root cur) (i + 1) cur res
-        | _ -> let (root, flag) = has_value root cur in
+        | _ -> let root, flag = has_value root cur in
                   if flag then loop root (i + 1) cur res + 1
                   else loop root (i + 1) cur res
   in loop None 1 5 0
