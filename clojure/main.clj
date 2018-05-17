90046b8fea3a93e29c894d4ddab3fbeba3fd2b86
Alexei Matveev <alexei.matveev@gmail.com>
2018-05-17 21:22:45 +0200

    AM: rename and comments

--- clojure/main.clj
+++ clojure/main.clj
@@ -3,7 +3,7 @@
 ;; Exponentiation:
 (defn ** [x n] (reduce * (repeat n x)))
 
-;; Why using PRNG below why still non-deterministic here?
+;; Deliberatly using PRNG below while still non-deterministic here?
 (defn make-random-node [x]
   {:x x
    :y (rand-int (** 2 31))
@@ -19,26 +19,26 @@
         (assoc lower :right (merge2 (:right lower) greater))
         (assoc greater :left (merge2 lower (:left greater)))))))
 
-(defn split-binary [orig value]
+(defn split2 [orig value]
   (if-not orig
     [nil nil]
     (if (< (:x orig) value)
-      (let [[fst snd] (split-binary (:right orig) value)]
+      (let [[fst snd] (split2 (:right orig) value)]
         [(assoc orig :right fst) snd])
-      (let [[fst snd] (split-binary (:left orig) value)]
+      (let [[fst snd] (split2 (:left orig) value)]
         [fst (assoc orig :left snd)]))))
 
 (defn merge3 [lower equal greater]
   (merge2 (merge2 lower equal) greater))
 
-(defn split [orig value]
-  (let [[lower equal-greater] (split-binary orig value)
-        [equal greater] (split-binary equal-greater (inc value))]
+(defn split3 [orig value]
+  (let [[lower equal-greater] (split2 orig value)
+        [equal greater] (split2 equal-greater (inc value))]
     [lower equal greater]))
 
 ;; Very questionable function ...
 (defn has-value [root x]
-  (let [[lower equal greater] (split root x)
+  (let [[lower equal greater] (split3 root x)
         new-root (merge3 lower equal greater)]
     ;; Why splitting to merge right afterwards? This seems to
     ;; be empirically true:
@@ -46,13 +46,13 @@
     [new-root equal]))
 
 (defn insert [root x]
-  (let [[lower equal greater] (split root x)]
+  (let [[lower equal greater] (split3 root x)]
     (merge3 lower
             (or equal (make-random-node x))
             greater)))
 
 (defn erase [root x]
-  (let [[lower equal greater] (split root x)]
+  (let [[lower equal greater] (split3 root x)]
     (merge2 lower greater)))
 
 (defn main [n]
