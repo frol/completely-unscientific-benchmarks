a0ce5de684a2302fc2f57b7d4c4b943530e31a2e
Paul Harvey <2677507+prharvey@users.noreply.github.com>
2018-05-16 20:37:34 -0700

    Update main-shared_ptr.cpp
    Added shared_ptr optimization as well.

--- c++/main-shared_ptr.cpp
+++ c++/main-shared_ptr.cpp
@@ -1,143 +1,111 @@
 #include <iostream>
 #include <memory>
 
-class Tree
-{
-public:
-    Tree() = default;
+class Treap {
+ public:
+  bool HasValue(int value);
+  void Insert(int value);
+  void Erase(int value);
 
-    bool hasValue(int x);
-    void insert(int x);
-    void erase(int x);
+ private:
+  struct Node {
+    explicit Node(int value) : value(value) {}
 
-private:
+    int value;
+    int priority = rand();
 
-    struct Node
-    {
-        Node(int x): x(x) {}
-        Node() {}
+    std::shared_ptr<Node> left;
+    std::shared_ptr<Node> right;
+  };
 
-        int x = 0;
-        int y = rand();
+  static void Split(std::shared_ptr<Node>&& input, int value,
+                    std::shared_ptr<Node>* less,
+                    std::shared_ptr<Node>* greater_or_equal);
+  static void Merge(std::shared_ptr<Node>&& less,
+                    std::shared_ptr<Node>&& greater,
+                    std::shared_ptr<Node>* result);
 
-        std::shared_ptr<Node> left;
-        std::shared_ptr<Node> right;
-    };
-
-    using NodePtr = std::shared_ptr<Node>;
-
-    static NodePtr merge(const NodePtr& lower, const NodePtr& greater);
-    static NodePtr merge(const NodePtr& lower, const NodePtr& equal, const NodePtr& greater);
-    static void split(const NodePtr& orig, NodePtr& lower, NodePtr& greaterOrEqual, int val);
-    static void split(const NodePtr& orig, NodePtr& lower, NodePtr& equal, NodePtr& greater, int val);
-
-    NodePtr mRoot;
+  std::shared_ptr<Node> root_;
 };
 
-bool Tree::hasValue(int x)
-{
-    NodePtr lower, equal, greater;
-    split(mRoot, lower, equal, greater, x);
-    bool res = equal != nullptr;
-    mRoot = merge(lower, equal, greater);
-    return res;
+inline bool Treap::HasValue(int value) {
+  std::shared_ptr<Node> less;
+  std::shared_ptr<Node> greater;
+  Split(std::move(root_), value, &less, &greater);
+  Split(std::move(greater), value + 1, &root_, &greater);
+  const bool has_value = root_ != nullptr;
+  Merge(std::move(less), std::move(root_), &root_);
+  Merge(std::move(root_), std::move(greater), &root_);
+  return has_value;
 }
 
-void Tree::insert(int x)
-{
-    NodePtr lower, equal, greater;
-    split(mRoot, lower, equal, greater, x);
-    if(!equal)
-        equal = std::make_shared<Node>(x);
-
-    mRoot = merge(lower, equal, greater);
+inline void Treap::Insert(int value) {
+  std::shared_ptr<Node> less;
+  std::shared_ptr<Node> greater;
+  Split(std::move(root_), value, &less, &greater);
+  Split(std::move(greater), value + 1, &root_, &greater);
+  if (!root_) root_ = std::make_shared<Node>(value);
+  Merge(std::move(less), std::move(root_), &root_);
+  Merge(std::move(root_), std::move(greater), &root_);
 }
 
-void Tree::erase(int x)
-{
-    NodePtr lower, equal, greater;
-    split(mRoot, lower, equal, greater, x);
-    mRoot = merge(lower, greater);
+inline void Treap::Erase(int value) {
+  std::shared_ptr<Node> less;
+  std::shared_ptr<Node> greater;
+  Split(std::move(root_), value, &less, &greater);
+  Split(std::move(greater), value + 1, &root_, &greater);
+  Merge(std::move(less), std::move(greater), &root_);
 }
 
-Tree::NodePtr Tree::merge(const NodePtr& lower, const NodePtr& greater)
-{
-    if(!lower)
-        return greater;
+inline void Treap::Split(std::shared_ptr<Node>&& input, int value,
+                         std::shared_ptr<Node>* less,
+                         std::shared_ptr<Node>* greater_or_equal) {
+  if (!input) {
+    *less = *greater_or_equal = nullptr;
+  } else if (input->value < value) {
+    const auto ptr = &input->right;
+    *less = std::move(input);
+    Split(std::move(*ptr), value, ptr, greater_or_equal);
+  } else {
+    const auto ptr = &input->left;
+    *greater_or_equal = std::move(input);
+    Split(std::move(*ptr), value, less, ptr);
+  }
+}
 
-    if(!greater)
-        return lower;
+inline void Treap::Merge(std::shared_ptr<Node>&& less,
+                         std::shared_ptr<Node>&& greater,
+                         std::shared_ptr<Node>* result) {
+  if (!less | !greater) {
+    *result = std::move(less ? less : greater);
+  } else if (less->priority < greater->priority) {
+    const auto ptr = &less->right;
+    Merge(std::move(*ptr), std::move(greater), ptr);
+    *result = std::move(less);
+  } else {
+    const auto ptr = &greater->left;
+    Merge(std::move(less), std::move(*ptr), ptr);
+    *result = std::move(greater);
+  }
+}
 
-    if(lower->y < greater->y)
-    {
-        lower->right = merge(lower->right, greater);
-        return lower;
+int main() {
+  srand(time(0));
+
+  Treap treap;
+  int current = 5;
+  int result = 0;
+  for (int i = 1; i < 1000000; ++i) {
+    const int mode = i % 3;
+    current = (current * 57 + 43) % 10007;
+    if (mode == 0) {
+      treap.Insert(current);
+    } else if (mode == 1) {
+      treap.Erase(current);
+    } else if (mode == 2) {
+      result += treap.HasValue(current);
     }
-    else
-    {
-        greater->left = merge(lower, greater->left);
-        return greater;
-    }
-}
-
-Tree::NodePtr Tree::merge(const NodePtr& lower, const NodePtr& equal, const NodePtr& greater)
-{
-    return merge(merge(lower, equal), greater);
-}
-
-void Tree::split(const NodePtr& orig, NodePtr& lower, NodePtr& greaterOrEqual, int val)
-{
-    if(!orig)
-    {
-        lower = greaterOrEqual = nullptr;
-        return;
-    }
-
-    if(orig->x < val)
-    {
-        lower = orig;
-        split(lower->right, lower->right, greaterOrEqual, val);
-    }
-    else
-    {
-        greaterOrEqual = orig;
-        split(greaterOrEqual->left, lower, greaterOrEqual->left, val);
-    }
-}
-
-void Tree::split(const NodePtr& orig, NodePtr& lower, NodePtr& equal, NodePtr& greater, int val)
-{
-    NodePtr equalOrGreater;
-    split(orig, lower, equalOrGreater, val);
-    split(equalOrGreater, equal, greater, val + 1);
-}
-
-int main()
-{
-    srand(time(0));
-
-    Tree tree;
-
-    int cur = 5;
-    int res = 0;
-
-    for(int i = 1; i < 1000000; i++)
-    {
-        int mode = i % 3;
-        cur = (cur * 57 + 43) % 10007;
-        if(mode == 0)
-        {
-            tree.insert(cur);
-        }
-        else if(mode == 1)
-        {
-            tree.erase(cur);
-        }
-        else if(mode == 2)
-        {
-            res += tree.hasValue(cur);
-        }
-    }
-    std::cout << res;
-    return 0;
+  }
+  std::cout << result;
+  return 0;
 }
