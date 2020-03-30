de621de621b0e694c92ad105efe91821b627f648
David Bishai <dbishai@outlook.com>
2020-03-29 19:17:02 -0500

    organize code and get rid of cond statements

---
+++ elixir/lib/treap.ex
@@ -0,0 +1,27 @@
+defmodule Treap do
+  defmodule TreapNode do
+    defstruct x: nil, y: :rand.uniform(), left: nil, right: nil
+  end
+
+  def has_value?(root, x) do
+    splitted = BulkOps.split(root, x)
+    res = splitted.equal != nil
+    root = BulkOps.merge3(splitted.lower, splitted.equal, splitted.greater)
+    {root, res}
+  end
+
+  def insert(root, x) do
+    splitted = BulkOps.split(root, x)
+
+    if splitted.equal == nil do
+      BulkOps.merge3(splitted.lower, %TreapNode{x: x}, splitted.greater)
+    else
+      BulkOps.merge3(splitted.lower, splitted.equal, splitted.greater)
+    end
+  end
+
+  def erase(root, x) do
+    splitted = BulkOps.split(root, x)
+    BulkOps.merge(splitted.lower, splitted.greater)
+  end
+end
